7c27ecd6a75f926753a38970bebe4d24e85d1696
Jorge <jorgesifontes.cl@gmail.com>
2021-01-24 12:46:53 -0300

    Requirement 6

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,10 +1,8 @@
 class Group < ApplicationRecord
-    has_many :concerts
+    has_many :concerts, dependent: :destroy
     validates :group_name, :num_of_members, :debut_date, :group_type, presence: true
 
     enum group_type: [:men, :women, :band]
-
-    
 
     def to_s
         group_name
