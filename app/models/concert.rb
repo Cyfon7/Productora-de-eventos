eb31e45a75cbc1cd0e847b95a22a95a3f6035cea
Jorge <jorgesifontes.cl@gmail.com>
2021-01-24 12:06:12 -0300

    Requirement 3

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -2,4 +2,6 @@ class Concert < ApplicationRecord
     belongs_to :group
     validates :concert_title, :num_of_attendants, :concert_date, :group_id, presence: true
     
+    #Count concerts for specific ID group
+    scope :concerts_of_group, -> (group_id) { Concert.group(:group_id).count[group_id] }
 end
