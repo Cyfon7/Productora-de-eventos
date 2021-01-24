8233b3413bb623ace1f9ff3fbf32311674420146
Jorge <jorgesifontes.cl@gmail.com>
2021-01-24 12:42:37 -0300

    Requirement 5

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -4,4 +4,7 @@ class Concert < ApplicationRecord
     
     #Count concerts for specific ID group
     scope :concerts_of_group, -> (group_id) { Concert.group(:group_id).count[group_id] }
+
+    #Totalize Attendands for specific ID group
+    scope :group_attendants, -> (group_id) { Concert.where(group_id: group_id).sum(:num_of_attendants)}
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -4,6 +4,8 @@ class Group < ApplicationRecord
 
     enum group_type: [:men, :women, :band]
 
+    
+
     def to_s
         group_name
     end
